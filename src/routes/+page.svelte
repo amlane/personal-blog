294c34db1daa114c5ed316fc2c2483bf50c59c8a
Amanda Lane <reallycoolcoder@gmail.com>
2026-08-12 21:33:04 -0500

    added hover effect and started basic styling OH I AM SO BACK

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	import Navbar from "./navbar.svelte";
 	import "../../css/main.css";
 </script>
-<div class="home">
+<div class="home center-page">
 <header>
 	<h1>Amanda Lane</h1>
 	<h2>Build | Write | Design</h2>
--- src/routes/navbar.svelte
+++ src/routes/navbar.svelte
@@ -1,6 +1,6 @@
-<nav>
-<ul>
-	<li><a href="/about">About</a></li>
-	<li><a href="/blog">Reflections</a></li>
-</ul>
+<nav class="main-nav">
+<ol>
+	<li><a href="/about">ABOUT</a></li>
+	<li><a href="/blog">REFLECTIONS</a></li>
+</ol>
 </nav>
