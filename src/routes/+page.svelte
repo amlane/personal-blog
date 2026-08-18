60251b42750970328465598d269410ae2f034b1e
Amanda Lane <reallycoolcoder@gmail.com>
2026-08-17 23:16:01 -0500

    minor UI tweaks to make footer same width as chapter nav

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,11 +4,11 @@
 	import HomeBtn from "./homebtn.svelte";
 </script>
 <div class="home center-page text-center">
-<header class="title">
-	<h1 class="text-xxxl py-16">Amanda Lane</h1>
-	<h2>Build | Write | Design</h2>
-</header>
-<Navbar />
-<Footer />
-<HomeBtn />
+	<header class="title">
+		<h1 class="text-xxxl py-16">Amanda Lane</h1>
+		<h2>Build | Write | Design</h2>
+	</header>
+	<Navbar />
+	<Footer />
+	<HomeBtn />
 </div>
