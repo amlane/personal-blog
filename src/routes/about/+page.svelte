c767fd5acf9f933c37cd52541ccf43cbe9ba49ae
Amanda Lane <reallycoolcoder@gmail.com>
2026-08-18 23:25:37 -0500

    update about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -5,8 +5,12 @@
 <div class="container center-page">
 <div class="about page-content">
 <h1 class="title text-center">About</h1>
-<p>Hey! I'm Amanda and I've spent the last 6 years as a software engineer leading development teams. I love solving problems, learning new tools, and being part of a collaborative environment. I want to spend my time here learning, growing and contributing meaningful ideas that help leave the world a better place than how I found it.</p>
-<p>My original passion for web dev started when I was using MySpace and LiveJournal. I came across profile designs that I didn't even know was possible and felt inspired. I started experimenting with HTML and CSS to create my own profiles, learning as I went. That is the inspiration behind the design of this site and I hope it gives you that same nostalgic feeling of the early 2000s web dev aesthetic.</p>
+<p>Once upon a time, in a far away land (Columbia, Missouri), lived a girl and her dog.</p>
+<p>Hey, I'm Amanda. Self-proclaimed Queen of bike trails (I do have a handful of QOMs on Strava). That's me and my dog, Prince Robert. When we're not out exploring nature, you can find me coding, reading or crafting.</p>
+<p>Like most people who grew up in the 90s, I was first introduced to code through websites like MySpace and LiveJournal. I remember discovering the code editor and seeing the different designs other people created. I loved how someone's website design could capture someone's essense and create an emotional response in the viewer, just like seeing a work of art. I would spend hours inspecting the code, building my own layouts and experimenting with different styles.</p>
+<p>I have a diverse career history. I've worked in several industries, including banking, sales, health/wellness, and higher education. Because of this, I bring a unique perspective to my work. I aim for my creations to be both functional and aesthetically pleasing.</p>
+<p>Most recently, I have been focusing on a career transition from engineering into product management. I am rediscovering my love for writing, both creative and technical. I look forward to sharing those journeys here as I document the process and how it unfolds.</p>
+<p>And she and her dog lived happily ever after (hopefully).</p>
 </div>
 <Footer />
 <Homebtn />
